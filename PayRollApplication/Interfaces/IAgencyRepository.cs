898794bedf55279e0256c52bccd04e4eb4122508
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2024-08-30 11:52:43 -0500

    Add Interfaces

---
+++ PayRollApplication/Interfaces/IAgencyRepository.cs
@@ -0,0 +1,20 @@
+﻿using PayRollApplication.Models.Agencies;
+using PayRollLibrary.Entities.Agencies;
+
+namespace PayRollApplication.Interfaces;
+
+public interface IAgencyRepository
+{
+    Task<List<AgencyModel>?> GetAllAgencies();
+    Task<AgencyModel?> GetAgencyByID(int agencyId);
+    Task InsertAgency(AgencyModel agency);
+    Task DeleteAgency(int agencyId);
+    Task UpdateAgency(AgencyModel agency);
+    Task<List<BranchModel>?> GetAllBranches();
+    Task<BranchModel?> GetBranchByID(int branchId);
+    Task InsertBranch(BranchModel branch);
+    Task DeleteBranch(int branchId);
+    Task UpdateBranch(BranchModel branch);
+    Task<BranchModelView?> DetailViewBranch(int branchId);
+    Task<List<BranchModelView>?> GetAllDetailViewBranches();
+}
---
+++ PayRollApplication/Models/Employees/EmployeeViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace PayRollApplication.Models.Employees;
+public class EmployeeViewModel
+{
+    public int Id { get; set; }
+    public int DocumentTypeId { get; set; }
+    public string? Document { get; set; }
+    public string? LastName { get; set; }
+    public string? SecondSurname { get; set; }
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public decimal BaseSalary { get; set; }
+    public string? ContractTypeName { get; set; }
+    public string? BranchName { get; set; }
+    public string? AgencyName { get; set; }
+    public string? PositionName { get; set; }
+    public DateTime CreationDate { get; set; }
+    public string? Photo { get; set; }
+    public bool IsValid { get; set; } = true;
+
+    public string? FullName
+    {
+        get => $"{LastName} {SecondSurname} {FirstName} {MiddleName}";
+    }
+}
